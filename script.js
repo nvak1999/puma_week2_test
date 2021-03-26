04aaa9efb3dda5b0321e3400ae79173022428cf3
luke <lukehoang195@gmail.com>
2021-03-26 16:27:52 +0700

    a

---
+++ script.js
@@ -0,0 +1,37 @@
+/** 
+ Hint :  function nameOfFunction(input1,input2,input3........){
+  step 1
+  step 2
+  step 3
+===> to print to the console : console.log() 
+===> to return any value     : return theValue
+}
+*/
+
+/** 
+ * Part1 (7)
+   Given an array text = ["text 1", "text 2", "text 3","text 4", "text 5"]
+      1- add "last text" to END the array 
+      2- print to the console all the texts
+      3- write a function to print to console random text of the array
+      4- enhance the function so that it take an number input that less than length of array
+      5- if the input is odd, print only ODD-position text
+      6- if the input is even, print only even-position text
+      7- if the input is zero, randomly assign new number and do step 5->6
+ */
+
+/**
+ * Part 2 (3)
+  Instead of printing to the console, let now print the result to the website (HTML document)
+  Make a button that when clicked :
+  1 : `The length of the array is : ....`
+  2 : `The number is : ..., it is : .... (odd/even)`
+  3 :  Make a button to random a number 
+        - then execute step 4 part 1
+        - then changing value of step 1,2, show to website
+*/
+
+/** Bounus:
+ * Do not create more files. Change all text color into anycolor but black
+ * Create another button that create another button that able to make another button
+ */
